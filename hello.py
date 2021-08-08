43b6c06082c11b286ac41ec18e6ffc6eef576c5f
Mohammad Zaman <mohammad.zaman@carrier.com>
2021-08-08 11:40:49 -0400

    added and testing new routes

--- hello.py
+++ hello.py
@@ -1,61 +1,96 @@
-from flask import request, Flask, render_template, session, url_for, redirect, flash
+from flask import Flask, session, render_template, url_for, redirect
 from NameForm import NameForm
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
+
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'MY_SECRET_KEY'
+basedir = os.path.abspath(os.path.dirname(__file__))  # /Users/Mohammad.Zaman/PycharmProjects/flasky
+print('basedir: ', basedir)
+# sqlite:////absolute/path/to/database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MY-SECRET-KEY'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = '587'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = "[FlaskY]"
+app.config['FLASKY_MAIL_SENDER'] = '<MOHAMMAD ZAMAN> mzaman.aws@gmail.com'
 
-bootstrap = Bootstrap(app)
+boostrap = Bootstrap(app)
+# The db object instantiated from the class SQLAlchemy
+# represents the database and provides access
+# to all the functionality of Flask-SQLAlchemy.
+db = SQLAlchemy(app)
+mail = Mail(app)
 
 
-@app.route('/name-form', methods=['GET', 'POST'])
-def name_form():
-    name = None
-    email = None
-    gender = None
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False, unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    # a role has many users -> one to many
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r >' % self.name
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     name_form = NameForm()
+    print('username:', name_form.name.data)
+    # checking if the user already exsits in the db
     if name_form.validate_on_submit():
-        name = name_form.name.data
-        email = name_form.email.data
-        gender = name_form.gender.data
-        print(f'name: {name} | email: {email}) |  gender: {gender}')
-        name_form.name.data = ''
-        name_form.email.data = ''
-        name_form.gender.data = ''
+        user = User.query.filter_by(username=name_form.name.data).first()
+        print('user', user)
+        if user is None:
+            user = User(username=name_form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            db.session.commit()
+        else:
+            session['known'] = True
+            session['name'] = user.username
+            name_form.name.data = ''
+            return redirect(url_for('display_user'))
 
-    return render_template('index.html', form=name_form, name=name, email=email, gender=gender)
-
-
-# storing form data in a session
-@app.route('/name-form-redirect-on-submit', methods=['GET', 'POST'])
-def name_form_redirect_on_form_submit():
-    print('*** request mehtod: ', request.method)
-    name_form = NameForm()
-
-    print('name_form.name.data: ', name_form.name.data)
-    print('name_form.email.data: ', name_form.email.data)
-    print('name_form.gender.data: ', name_form.gender.data)
-
-    if request.method == 'GET':
-        print('clearing out form data')
-        session['name'] = ''
-        session['gender'] = ''
-        session['email'] = ''
-
-    print(f'*** endpoint: name_form_redirect_on_form_submit')
-    print(f'*** Before form submitted session: {session}')
-    if name_form.validate_on_submit():
-        session['name'] = name_form.name.data
-
-        session['gender'] = name_form.gender.data
-        session['email'] = name_form.email.data
-        return redirect(url_for('display_user_info'))
-
-    print(f'*** after form submitted session: {session}')
-    return render_template('index.html', form=name_form, name=session.get('name'), my_session=session)
+    return render_template('index.html', name=session.get('name'), form=name_form)
 
 
 @app.route('/user-info')
-def display_user_info():
-    print(f'*** *** *** user-info page- request.session: {session}')
+def display_user():
     return render_template('user.html', my_session=session)
+
+
+# @app.shell_context_processors
+# def make_shell_context():
+#     return dict(db=db, User=User, Role=Role)
+
+
+@app.route('/mail/<to>', methods=['POST'])
+def send_mail(to):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + 'flask-mail',
+                  sender=app.config['FLASKY_MAIL_SENDER'])
+    msg.body('this is a plain text body')
+    msg.body('this is a plain <b>HMTL</b> body')
+    msg.subject('flask-mail')
+    mail.send(msg)
--- main.py
+++ main.py
@@ -1,74 +1,27 @@
-import json
+from flask import Flask, request, jsonify
 
-from flask import Flask, request, current_app, render_template
-
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', name="ALC- Autobots ")
+@app.route('/json')
+def get_json():
+    person = {'name': 'Jamal', 'age': 23, 'gender': 'male', 'isActive': True}
+    return jsonify(person)
 
 
+@app.route('/query-person')
+def using_query_string():
+    name = request.args.get('name')
+    gender = request.args.get('gender')
+    age = request.args.get('age')
+    is_active = request.args.get('isActive') in ['1', 'true', 'True']
+    print(f'Type of is_active {type(is_active)} : value: {is_active}')
+    print(f'bool(is_active): {bool(is_active)}')
+    person_dict = {
+        'name': name,
+        'age': age,
+        'gender': gender,
+        'isActive': is_active
+    }
 
-@app.route('/user/<name>')
-def user(name):
-    return f'Hello, user: {name}'
-
-
-@app.route('/user/<int:id>')
-def user_by_id(id):
-    return f'found user for id: {id}'
-
-
-@app.route('/user/dob/<dob>')
-def user_by_dob(dob):
-    return f'found user wiht DOB: {dob}'
-
-
-@app.route('/print-request-object')
-def print_request_object():
-    print('request object is an Instance of Request class: ', 'https://docs.python-requests.org/en/master/api/')
-
-    print(f'*** request object class: {request.__class__}')
-    print(f'*** request object repr: {request.__repr__()}')
-    print(f'*** Object type request: {type(request)}')
-    print(f'*** request-useragent: {request.user_agent}')
-    print(f"*** request.headers.get(User-agent): {request.headers.get('User-Agent')}")
-    print(f'request.method: {request.method}')
-    print(f'request.cookies: {request.cookies} and Length of cookies: {len(request.cookies)}')
-    print(f'request.data: {request.data}')
-    print(f'request.url: {request.url}')
-    print(f'get_json: {request.get_json()}')
-    url_map_str = str(current_app.url_map)
-    print('url_map_str', url_map_str)
-    url_map_list = url_map_str.split('>,')
-    request_values = f'*** request.values type: {type(request.values)} |  size:  {len(request.values)} | and values: {request.values}'
-    request_form = f'*** request.form type: {type(request.form)} | size : {len(request.form)} | form : {request.form}'
-    print(request_values)
-    print(request_form)
-    return {
-        'body': {
-            'request-object': 'https://flask.palletsprojects.com/en/2.0.x/api/#flask.Request',
-            'User-Agent': request.headers.get('User-Agent'),
-            'json': request.get_json(),
-            'url-map': url_map_list,
-            'request_values': request_values,
-            'request_form': request_form,
-            'remote_addr': request.remote_addr,
-            'remote_user': request.remote_user,
-            'is_secure': request.is_secure,
-            'endpoint[request-handler / method-name]': request.endpoint
-        }
-    }, 200
-
-
-@app.route('/test-current-app')
-def test_current_app():
-    print(f'current_app: {current_app.name}')
-    app_ctx = app.app_context()
-    app_ctx.push()
-    print(f'current_app: {current_app.name}')
-    app_ctx.pop()
-    return 'testing current app'
-
+    return jsonify(person_dict)
