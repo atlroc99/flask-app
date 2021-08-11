1ddc473bb00536c77a350fe77a5e1d2fe76806ee
Mohammad Zaman <mohammad.zaman@carrier.com>
2021-08-11 09:57:48 -0400

    added and cofigured sql-alchamey

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from werkzeug.wrappers import request
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'dd6b2df8ca878fabec08548731962fa581dfce57118803f55cf3b56e26537b86'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 posts = [
     {
